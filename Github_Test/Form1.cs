7246d9baa6fa83eefcb130434b7d7b687814b0c5
RDonaldson01 <rrdonaldson01@gmail.com>
2024-02-02 11:27:58 -0600

    Added Hello World

--- Github_Test/Form1.cs
+++ Github_Test/Form1.cs
@@ -6,5 +6,10 @@ namespace Github_Test
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
